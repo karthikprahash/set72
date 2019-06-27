8d64dfbda493528ce9a4df4385da3b4101b375d2
Karthik Prahash.M <46716288+karthikprahash@users.noreply.github.com>
2019-06-27 20:49:59 +0530

    Update and rename README.md to set72.py

---
+++ set72.py
@@ -0,0 +1,16 @@
+# set72
+i=(input())
+f=1
+for x in range(len(i)):
+    
+    if i[x]=='0'or i[x]=='1':
+         f=1
+    else:
+        f=0
+if f==1:
+    print('yes')
+else:
+    print('no')
+    
+ 
+   
